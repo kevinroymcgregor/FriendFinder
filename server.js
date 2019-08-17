cdbb5710bd379014e5f318ab3b8f428ad979d99e
kevinroymcgregor <kevin.mcgregor85204@gmail.com>
2019-08-16 22:59:10 -0700

    setup server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(parser.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/app/public")));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("FriendFinder listening on PORT " + PORT);
+});
