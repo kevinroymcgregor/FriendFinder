08d967a8cdb53eca8fc9734050d4b7fea764ee12
kevinroymcgregor <kevin.mcgregor85204@gmail.com>
2019-08-17 21:15:33 -0700

    Survey page created

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(parser.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "/app/public")));
 
-require("./app/routing/apiRoutes.js")(app);
+// require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
